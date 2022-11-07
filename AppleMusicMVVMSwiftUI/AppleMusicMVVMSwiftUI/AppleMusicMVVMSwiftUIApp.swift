a5cd6c19e84e1d4b65ab67f732976e9fc05c6011
Brandon Gouws <bgouws@jhb.dvt.co.za>
2022-11-07 19:35:19 +0200

    Initial commit

---
+++ AppleMusicMVVMSwiftUI/AppleMusicMVVMSwiftUI/AppleMusicMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicMVVMSwiftUIApp.swift
+//  AppleMusicMVVMSwiftUI
+//
+//  Created by Brandon Gouws on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
